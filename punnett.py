f2e0d66245ee88ba13e6a9db83cdb3be368822d4
ddmin <danieldmin216@gmail.com>
2022-01-31 11:11:06 -0500

    basic punnett square

--- punnett.py
+++ punnett.py
@@ -3,12 +3,18 @@ from rich.table import Table
 
 table = Table(show_lines=True)
 
-table.add_column("", justify="right", style="cyan", no_wrap=True)
-table.add_column("X", justify="center", style="magenta")
-table.add_column("X", justify="center", style="green")
+geno1 = input("Genotype of Parent 1: ")
+geno2 = input("Genotype of Parent 2: ")
 
-table.add_row("X", "XX", "XX")
-table.add_row("Y", "XY", "XY")
+assert len(geno1) == len(geno2), "ERROR: Gametes have different lengths"
+
+table.add_column("", justify="right")
+
+for allele in geno1:
+    table.add_column(allele, justify="center")
+
+for allele in geno2:
+    table.add_row(allele, f"{geno1[0]}{allele}", f"{geno1[1]}{allele}")
 
 console = Console()
 console.print(table)
